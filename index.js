64e71f51f8c408573057ff22403ab3b76b7e6079
xaviguasch <xaviguasch@gmail.com>
2020-08-19 21:44:55 +0200

    solved third and final challenge

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 /* 
-    2. 
-       Write a function to create the my-photos Component
-       
-       Create a Column Flexbox container around my-photos
-       
-       Create a new img and place it, centered, above my-photos
-        -This should be hidden initially
-        -Also give it the my-photo class
-    
-       Create a "click" event for each photo in my-photos
-        -When clicked, load the clicked photo into the new img tag
+    Challenge:
+        
+        Reverse the Column's direction in my-gallery
+            Such that my-photos appear over #my-selected-photo
+        
+        Change the size of #my-selected-photo to 300x300
+            And set its default src to be the 3rd item in the Photos array
+            
+        Align the photos in my-photos to the right, using Flexbox
+            Align #my-selected-photo to the left, using Flexbox
 */
 
 async function getPhotos() {
@@ -18,27 +17,27 @@ async function getPhotos() {
   return photos
 }
 
-function getPhotosHtml(photos) {
+function getMyPhotosHtml(photos) {
   let myPhotosHtml = photos
     .map((photo) => {
       return `<img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photo.title}"/>`
     })
     .join('')
-
   return `<div class="my-photos">${myPhotosHtml}</div>`
 }
 
 getPhotos().then((photos) => {
+  let myPhotosHtml = getMyPhotosHtml(photos)
   document.body.innerHTML = `<div class="my-gallery">
-      <img style="display: none;" class="my-photo" id="my-selected-photo" src="https://picsum.photos/id/1/200/200" />
-      ${getPhotosHtml(photos)}
+      <img id="my-selected-photo" class="my-photo"
+      src="https://picsum.photos/id/${photos[2].id}/300/300" />
+      ${myPhotosHtml}
   </div>`
 
   let myPhotoImgs = Array.from(document.getElementsByClassName('my-photo'))
   myPhotoImgs.forEach((photoImg) => {
-    photoImg.addEventListener('click', (event) => {
-      let selectedPhotoSrc = `${photoImg.src.substr(0, photoImg.src.length - 7)}200/200`
-
+    photoImg.addEventListener('click', (e) => {
+      let selectedPhotoSrc = photoImg.src.substr(0, photoImg.src.length - 7) + `300/300`
       let selectedPhoto = document.getElementById('my-selected-photo')
       selectedPhoto.src = selectedPhotoSrc
       selectedPhoto.style.display = 'inline'
