c0378fa8e0827b80dacc08965d5f713fe672bad5
xaviguasch <xaviguasch@gmail.com>
2020-08-19 12:11:39 +0200

    added initial files

---
+++ index.js
@@ -0,0 +1,16 @@
+/* 
+    1. 
+        Use fetch() to load data from photos.json
+        
+        Display the photos inside of a FlexBox container
+            The src will be https://picsum.photos/id/${photo.id}/100/100
+            The alt text will be the photo.title from the json
+
+        Use Flexbox to display the photos in a Row 
+            The Row should Wrap to a new line on overflow
+            Give each photo a white 0.1rem solid border
+            Rounded corners at 0.7rem border-radius
+            Each photo should be at least 1rem away from the photos around it
+*/
+
+console.log('testing!')
